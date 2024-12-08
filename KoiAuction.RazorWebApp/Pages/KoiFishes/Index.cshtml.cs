e5dcd1f27725d4861389aab8d57e4b63fb8ae21d
EmBerrrrrr <lan18051973@gmail.com>
2024-12-08 21:40:19 +0700

    Update KoiFish

---
+++ KoiAuction.RazorWebApp/Pages/KoiFishes/Index.cshtml.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using KoiAuction.Repositories.Models;
+using KoiAuction.Services;
+
+namespace KoiAuction.RazorWebApp.Pages.KoiFishes
+{
+    public class IndexModel : PageModel
+    {
+        private readonly KoiFishService _koiFishService;
+
+        public IndexModel()
+        {
+            _koiFishService = new KoiFishService();
+        }
+
+        // Thuộc tính cho form tìm kiếm
+        public string? Name { get; set; }
+        public string? Status { get; set; }
+        public string? Gender { get; set; }
+
+        public int CurrentPage { get; set; }
+        public int TotalPages { get; set; }
+        public int PageSize { get; set; } = 3;
+
+        // Danh sách KoiFish để hiển thị
+        public List<KoiFish> KoiFish { get; set; } = new List<KoiFish>();
+
+        public async Task OnGetAsync(string? name, string? status, string? gender, int pageNumber = 1)
+        {
+            List<KoiFish> fishs;
+            List<KoiFish> allFishs;
+            // Gán giá trị từ query string vào các thuộc tính
+            Name = name;
+            Status = status;
+            Gender = gender;
+
+            fishs = await _koiFishService.SearchAsync(name, status, gender);
+            allFishs = await _koiFishService.GetAllWith();
+
+            if (fishs.Count > 0)
+            {
+                KoiFish = fishs;
+            }
+            else
+            {
+                KoiFish = allFishs;
+            }
+            var totalItems = KoiFish.Count();
+            TotalPages = (int)Math.Ceiling(totalItems / (double)PageSize);
+            CurrentPage = pageNumber;
+
+            KoiFish = KoiFish
+                .Skip((pageNumber - 1) * PageSize)
+                .Take(PageSize)
+                .ToList();
+        }
+    }
+}
---
+++ KoiAuction.Services/KoiFishService.cs
@@ -0,0 +1,51 @@
+﻿using KoiAuction.Repositories.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiAuction.Services
+{
+    public class KoiFishService
+    {
+        private KoiAuction.Repositories.KoiFishRepository _repository;
+        public KoiFishService()
+        {
+            _repository = new KoiAuction.Repositories.KoiFishRepository();
+        }
+        public async Task<List<KoiFish>> GetAll()
+        {
+            return await _repository.GetAllAsync();
+        }
+        public async Task<List<KoiFish>> GetAllWith()
+        {
+            return await _repository.GetGroupAll();
+        }
+        public async Task<KoiFish> GetByIdWithIncludesAsync(int? id)
+        {
+            return await _repository.GetByIdWithIncludesAsync(id);
+        }
+        public async Task<int> Create(KoiFish koifish)
+        {
+            return await _repository.CreateAsync(koifish);
+        }
+        public async Task<KoiFish> GetById(int id)
+        {
+            return await _repository.GetByIdAsync(id);
+        }
+        public async Task<int> UpdateAsync(KoiFish koifish)
+        {
+            return await _repository.UpdateAsync(koifish);
+        }
+        public async Task<bool> Delete(KoiFish koifish)
+        {
+            return await _repository.RemoveAsync(koifish);
+        }
+        public async Task<List<KoiFish>> SearchAsync(string? name, string? status, string? gender)
+        {
+            return await _repository.SearchAsync(name, status, gender);
+        }
+    }
+
+}
